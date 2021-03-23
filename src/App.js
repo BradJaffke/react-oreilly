f24fb003d1f0848d704a12605d1580513e3d1f0e
n0209516 <brad.jaffke@libertymutual.com>
2021-03-23 15:59:27 -0700

    End of day 3-23

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
+class App extends Component {
+    state = {
+        persons: [
+            {name: 'Brad', age: 32},
+            {name: 'Brittany', age: 30}
+        ]
+    }
+
+    switchNameHandler = () => {
+        console.log('Was clicked');
+    }
+
+    render() {
         return (
-          <div className="App">
-            <h1>Hi, I'm a React App</h1>
-          </div>
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <p>This is really working!</p>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>TEST TEST</Person>
+            </div>
         );
+    }
 }
 
 export default App;
